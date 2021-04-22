d550a83bbacd0c76f365ab5cf25be29a63351b0c
Dorian <dorian.marques.2e7@gmail.com>
2021-04-22 15:01:00 +0200

    add UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App d-flex flex-column justify-content-center align-items-center">
+      <h1>Blockchain UI</h1>
+      <div className="card w-75">
+        <div className="card-header">
+          Featured
+        </div>
+        <div className="card-body">
+          <h5 className="card-title">Check Status</h5>
+          <p className="card-text">Check current command status</p>
+          <a onClick={() => alert("Désolé mais ça marche pas !")} className="btn btn-primary">Check</a>
+        </div>
+      </div>
     </div>
   );
 }
